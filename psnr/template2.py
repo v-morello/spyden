fb7057284746e1e42fe460268c7b80d3ee3772dd
Vincent Morello <vmorello@gmail.com>
2019-11-12 16:29:40 +0000

    A much better Template class, with just-in-time padding

---
+++ psnr/template2.py
@@ -0,0 +1,260 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import log, pi, sin, sqrt, ceil, exp
+
+
+def normalise(data):
+    """
+    Normalise data to zero-mean and unit square sum
+
+    Parameters
+    ----------
+    data: ndarray, 1D
+
+    Returns
+    -------
+    normalised: ndarray, 1D
+    """
+    m = data.mean()
+    # This gives the square sum of (data - m)
+    sqsum = data.var(ddof=0) * data.size
+    return (data - m) * sqsum**-0.5
+
+
+class Template(object):
+    """ 
+    TODO
+    """
+    def __init__(self, data, pad_value=0.0, refbin=0, reference='start'):
+        # TODO: check input
+        self._data = data
+        self._pad_value = float(pad_value)
+        self._refbin = int(refbin) # refin must be in [0, data.size[
+        self._reference = str(reference)
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def pad_value(self):
+        return self._pad_value
+
+    @property
+    def refbin(self):
+        return self._refbin
+
+    @property
+    def reference(self):
+        return self._reference
+
+    def prepared_data(self, n):
+        """
+        Returns the pulse template padded to n bins, ready to be FFT convolved
+        with some input data with n bins.
+
+        The template is:
+        - padded to a total of n bins by appending its specified 
+          pad value on the right, 
+        - circularly shifted to place the reference bin at index 0
+        - properly time-reversed so that the circular convolution theorem 
+          correctly applies
+        - normalised to zero mean and unit square sum
+        - cast to np.float32 to save computation time later on
+
+        Parameters
+        ----------
+        n: int
+            The total length to which the template must be padded. This will
+            generally be a power of two to compute FFTs faster.
+        """
+        if not n >= self.size:
+            raise ValueError("Cannot pad template data to length shorter than template size")
+
+        # Pad to a total of n bins by appending 'pad_value' on the right
+        x = np.pad(
+            self.data, 
+            (0, n - self.size), # pad on right size up to length n
+            mode='constant', 
+            constant_values=(self.pad_value, self.pad_value))
+
+        # Place reference bin at index 0
+        x = np.roll(x, -self.refbin)
+
+        # We want to get the array y such that: y[k] = x[-k]
+        # NOTE: this is NOT like reversing the array
+        # since y[0] = x[0]
+        x = np.roll(x[::-1], 1)
+        return normalise(x).astype(np.float32)
+
+    @classmethod
+    def boxcar(cls, w):
+        """ 
+        Generate a boxcar pulse template with given width, expressed in number
+        of bins.
+
+        Parameters
+        ----------
+        w: int
+            Width in number of bins
+
+        Returns
+        -------
+        t: Template
+            A boxcar pulse template. The reference bin is located at the start
+            of the boxcar, that is bin index 0.
+        """
+        if not isinstance(w, int):
+            raise ValueError("w must be of type int")
+        if not w > 0:
+            raise ValueError("w must be strictly positive")
+        return cls(np.ones(w), pad_value=0.0, refbin=0, reference='start')
+
+    @classmethod
+    def gaussian(cls, w):
+        """
+        Generate a Gaussian pulse template with given pulse FWHM, expressed
+        in number of bins. The total number of bins in the template is chosen
+        so that the range [-3.5 sigma, +3.5 sigma] is covered, where
+
+        sigma = w / (2 * sqrt(2 * log(2)))
+
+        giving a total of approximately 7 x sigma bins, with a minimum of 3.
+
+        Parameters
+        ----------
+        w: float
+            FWHM in number of bins. Non-integer values are accepted.
+
+        Returns
+        -------
+        t: Template
+            A Gaussian pulse template. The reference bin is located at the peak
+            of the Gaussian.
+        """
+        try:
+            w = float(w)
+        except:
+            raise ValueError("Could not convert w to float")
+
+        if not w > 0:
+            raise ValueError("w must be strictly positive")
+
+        sigma = w / (2 * sqrt(2 * log(2)))
+        xmax = int(ceil(3.5 * sigma))
+        x = np.arange(-xmax, xmax + 1)
+        data = exp(-x**2 / (2 * sigma**2))
+        return cls(data, pad_value=data[-1], refbin=len(x)//2, reference='peak')
+
+    def plot(self, dpi=100):
+        """
+        Make a nice plot of the Template
+
+        Returns
+        -------
+        fig: matplotlib.Figure
+        """
+        fig = plt.figure(figsize=(6, 4.5), dpi=dpi)
+        plt.bar(range(self.size), self.data, width=0.9)
+
+        ymin, ymax = plt.ylim()
+        plt.plot(
+            [self.refbin, self.refbin], 
+            [ymin, ymax], 
+            linestyle='--', color='#ff9900', lw=2.0, label='Reference Bin')
+        plt.ylim(ymin, ymax)
+
+        plt.xlabel("Bin Index")
+        plt.ylabel("Amplitude")
+        plt.grid(linestyle=':')
+        plt.legend(loc='upper right')
+
+        title = "Template bins = {}".format(self.size)
+        plt.title(title)
+        plt.tight_layout()
+        return fig
+
+
+class TemplateBank(object):
+    """ 
+    TODO
+    """
+    def __init__(self, templates):
+        if not templates:
+            raise ValueError("templates is empty")
+
+        if not all(isinstance(t, Template) for t in templates):
+            raise ValueError("All input elements must be of Template instances")
+
+        self._templates = list(templates)
+
+    @property
+    def templates(self):
+        return self._templates
+
+    @property
+    def ntemp(self):
+        return len(self.templates)
+
+    @property
+    def maxsize(self):
+        return max(t.size for t in self.templates)
+
+    @classmethod
+    def boxcars(cls, widths):
+        """
+        Generate a TemplateBank with boxcars of specified widths. The templates
+        will be stored stored in *increasing* width order.
+
+        Parameters
+        ----------
+        widths: iterable
+            List or iterable of ints
+
+        Returns
+        -------
+        tbank: TemplateBank
+        """
+        templates = [
+            Template.boxcar(w) 
+            for w in sorted(widths)
+            ]
+        return cls(templates)
+
+    @classmethod
+    def gaussians(cls, widths):
+        """
+        Generate a TemplateBank with Gaussians of specified widths. The 
+        templates will be stored stored in *increasing* width order.
+
+        Parameters
+        ----------
+        widths: iterable
+            List or iterable of numbers (both int and float accepted)
+
+        Returns
+        -------
+        tbank: TemplateBank
+        """
+        templates = [
+            Template.gaussian(w) 
+            for w in sorted(widths)
+            ]
+        return cls(templates)        
+
+    def prepared_data(self, n):
+        """
+        Returns the pulse templates padded to n bins, ready to be FFT convolved
+        with some input data with n bins. The output is a 2D array. See
+        Template.prepared_data() for details.
+        """
+        return np.asarray([t.prepared_data(n) for t in self.templates])
+
+
+if __name__ == '__main__':
+    data = np.ones(3)
+    t = Template(data, pad_value=0.0, refbin=0, reference='start')
