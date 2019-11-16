023a5af0db3ffcbb95820dd9d79dfd3d4d68cd27
Vincent Morello <vmorello@gmail.com>
2019-11-16 09:52:56 +0000

    Define __all__ and __version__ in module init

--- psnr/__init__.py
+++ psnr/__init__.py
@@ -1,4 +1,7 @@
+from ._version import __version__
 from .template import Template, TemplateBank
 from .noisestats import noise_std
 from .snr import snratio
 from .tests import test
+
+__all__ = ['__version__', 'Template', 'TemplateBank', 'noise_std', 'snratio', 'test']
