c8fff2bcd8a765ec04789d0d8a0c508b61ac225e
Vincent Morello <vmorello@gmail.com>
2025-06-07 10:34:58 +0100

    Use src layout

--- src/spyden/__init__.py
+++ src/spyden/__init__.py
@@ -2,6 +2,5 @@ from ._version import __version__
 from .template import Template, TemplateBank
 from .noisestats import noise_mean, noise_std
 from .snr import snratio
-from .tests import test
 
-__all__ = ['__version__', 'Template', 'TemplateBank', 'noise_mean', 'noise_std', 'snratio', 'test']
+__all__ = ['__version__', 'Template', 'TemplateBank', 'noise_mean', 'noise_std', 'snratio']
